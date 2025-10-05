f2f161cea49bcc4df26217e76386ecfe8934140c
Daniel Lopez <dlope121@asu.edu>
2025-10-05 16:47:26 -0700

    Solved 2 Koan lessons, (command subsitution, arthimetric evulation, if, else, variable, directory, file checks) and added comments explaining each koan

--- src/00_about_arithmetics.sh
+++ src/00_about_arithmetics.sh
@@ -6,11 +6,16 @@ test_arithmetic_evaluation() {
 
 # shellcheck disable=SC2116
   output=$(echo 1+1)
+# 'echo 1+1' is executed and the result is given to 'output'
 
-  assertEqual "$output" __
+# 'assertEqual' is checking if variable 'output' and '__' are equal
+# This is verifying that 'output' matches the expected value, thus checking if command substitution worked
+  assertEqual "$output" '1+1'
 
+# Perform arthimetric evulation '1+1' and store in 'output2'
   output2=$((1+1))
 
-  assertEqual $output2 __
+# This is verifying that 'output2' matches the expected value, thus checking if arthimetric evulation command substitution worked
+  assertEqual $output2 2
 
 }
--- src/00_about_conditionals.sh
+++ src/00_about_conditionals.sh
@@ -2,76 +2,105 @@ lesson_title "Conditions"
 
 test_if_condition() {
 
+# Assigns 'ok' to local variable 'test'
   local test='ok'
 
+# Check if variable 'test' equals 'ok'
+# If true, assign 'YES' to variable 'assert'
   if [ $test = 'ok' ]; then
     local assert='YES'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'YES', which confirms the if condition executed correctly
+  assertEqual $assert 'YES'
 }
 
 
 test_if_condition_with_else() {
 
+# Assigns 'nope' to local variable 'test'
   local test='nope'
 
+# Check if variable 'test' equals 'ok'
+# If true, assign 'YES' to variable 'assert'
+# Else, assign 'NO' to variable 'assert'
   if [ $test == 'ok' ]; then
     local assert='YES'
   else
     local assert='NO'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'NO', which confirms the if-else condition executed correctly
+  assertEqual $assert 'NO'
 }
 
 test_if_condition_with_variables() {
+# Assigns 'OMG' to local variable 'variable'
+# Assigns 'OMG' to local variable 'condition'
   local variable="OMG"
   local condition='OMG' #__
 
+# Check if variable 'variable' equals variable 'condition'
+# If true, assign 'ok' to variable 'assert'
   if [  "$variable" = "$condition" ]; then
     local assert='ok'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'ok', which confirms the if condition executed correctly
+  assertEqual $assert 'ok'
 
 }
 
 test_multiple_if_conditions() {
 
+# Assigns 'zomg' to local variable 'test'
   local test='zomg' # __
 
+# Check if variable 'test' equals 'ok'
+# If true, assign 'no' to variable 'assert'
+# Else If, check if variable 'test' equals 'zomg'
+# If true, assign 'YES' to variable 'assert'
   if [ $test = 'ok' ]; then
     local assert='no'
   elif [ $test = 'zomg' ]; then
     local assert='YES'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'YES', which confirms the if-elif condition executed correctly
+  assertEqual $assert 'YES'
 
 }
 
 test_directory_if_conditions() {
+# Check if 'src' directory exists
+# If true, assign 'yes' to 'assert'
   if [ -d src ]; then
     local assert='yes'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'yes', which confirms the directory existence check
+  assertEqual $assert 'yes'
 
+# Check if nonexistrnt directory does NOT exists
+# If true, assign 'no' to 'assert'
   if [ ! -d NOT_EXISTENT_DIR ]; then
     local assert='no'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'no', which confirms the nondirectory existence check
+  assertEqual $assert 'no'
 
 }
 
 test_file_if_conditions() {
+# Check if 'README.md' file exists
+# If true, assign 'yes' to 'assert'
   if [ -f README.md ]; then
     local assert='yes'
   fi
 
-  assertEqual $assert __
+# This is verifying that 'assert' is 'yes', which confirms the file existence check
+  assertEqual $assert 'yes'
 
 }
 
