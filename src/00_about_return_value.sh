d6343e1df429642a8263784d218c9a7f8e9c499e
Daniel Lopez <dlope121@asu.edu>
2025-10-08 14:56:10 -0700

    From Return value Bash koan, solved the rest of test_return_value() function and add comments explaining each step

--- src/00_about_return_value.sh
+++ src/00_about_return_value.sh
@@ -9,7 +9,11 @@ test_return_value() {
   # since the cd failed, ($?) will be 1 (nonzero indicates failure), and 0 is a success.
   assertEqual $? 1
 
+  # Trys to list the contents of the home directory (~)
+  # (>) redirects stdout to /dev/null so we won't see the list
   ls ~/ > /dev/null
 
-  assertEqual $? __
+  # ($?) contains the exit status of the previous command and
+  # since the ls succeeded, ($?) will be 0, and (nonzero indicates failure).
+  assertEqual $? 0
 }
