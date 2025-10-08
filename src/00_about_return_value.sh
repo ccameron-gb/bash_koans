299604f66093d2dcb64113d19d2e5c559e6036a5
Daniel Lopez <dlope121@asu.edu>
2025-10-08 14:54:42 -0700

    From Return value Bash koan, solved 1st part of test_return_value() function and add comments explaining each step

--- src/00_about_return_value.sh
+++ src/00_about_return_value.sh
@@ -1,9 +1,13 @@
 lesson_title "Return value"
 
 test_return_value() {
+  # Trys to change into directory that doesn't exist and
+  # (&>) redirects both stdout and stderr to /dev/null so we won't see an error message
   cd /ZOMGNODIRLIKETHIS &> /dev/null
 
-  assertEqual $? __
+  # ($?) contains the exit status of the previous command and
+  # since the cd failed, ($?) will be 1 (nonzero indicates failure), and 0 is a success.
+  assertEqual $? 1
 
   ls ~/ > /dev/null
 
