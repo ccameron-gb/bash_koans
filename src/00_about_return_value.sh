398d002ee12c980eda1a4b54d5fd8b50a9c56342
dylan nguyen <dylan.nguyen@grey-box.ca>
2025-10-09 22:01:48 -0700

    Finalized version of return values lesson

--- src/00_about_return_value.sh
+++ src/00_about_return_value.sh
@@ -3,9 +3,9 @@ lesson_title "Return value"
 test_return_value() {
   cd /ZOMGNODIRLIKETHIS &> /dev/null
 
-  assertEqual $? __
+  assertEqual $? 1        # This is a valid case of entering the directory of ZOMGNODIRLLIKETHIS
 
-  ls ~/ > /dev/null
+  ls ~/ > /dev/null       # This is 0 because the syntax error contains in the slash before the ~
 
-  assertEqual $? __
+  assertEqual $? 0
 }
